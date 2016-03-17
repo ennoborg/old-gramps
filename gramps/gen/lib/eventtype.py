fca84e56921eaf9965fb02765c3947115defcb9e
Enno Borgsteede <ennoborg@gmail.com>
2016-03-17 19:58:54 +0100

    Moved CHR to the main events.

--- gramps/gen/lib/eventtype.py
+++ gramps/gen/lib/eventtype.py
@@ -141,12 +141,12 @@ class EventType(GrampsType):
         return value
 
     _MENU = [[_T_('Life Events'),
-              [BIRTH, BAPTISM, DEATH, BURIAL, CREMATION, ADOPT]],
+              [BIRTH, CHRISTEN, DEATH, BURIAL, CREMATION, ADOPT]],
             [_T_('Family'),
               [ENGAGEMENT, MARRIAGE, DIVORCE, ANNULMENT, MARR_SETTL, MARR_LIC,
                MARR_CONTR, MARR_BANNS, DIV_FILING, MARR_ALT]],
             [_T_('Religious'),
-              [CHRISTEN, ADULT_CHRISTEN, CONFIRMATION, FIRST_COMMUN, BLESS,
+              [BAPTISM, ADULT_CHRISTEN, CONFIRMATION, FIRST_COMMUN, BLESS,
                BAR_MITZVAH, BAS_MITZVAH, RELIGION]],
             [_T_('Vocational'),
               [OCCUPATION, RETIREMENT, ELECTED, MILITARY_SERV, ORDINATION]],
